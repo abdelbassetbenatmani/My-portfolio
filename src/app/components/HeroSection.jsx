38cfc7651def5d3f2b32332c57bfd4f833535955
abdelbassetbenatmani <abdelbassetbenatmani@gmail.com>
2023-12-16 20:05:15 +0100

    my portfolio second

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I'm Abdelbasset Benatmani full stack developer with more than 2 years.
+            I&apos;m Abdelbasset Benatmani full stack developer with more than 2 years.
           </p>
           <div>
             <Link
